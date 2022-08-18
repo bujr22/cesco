6bdf8990ae7ba952bc1bb0c117f1ed464f53f01b
bujr22 <107092944+bujr22@users.noreply.github.com>
2022-08-18 09:11:06 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,105 @@
+ // section1
+$('#consulting #textWrap div span').css({color:'#4096d4'})
+$('#consulting #textWrap div:eq(0) ').animate({opacity:1},800)
+$('#consulting #textWrap div:eq(1) ').delay(300).animate({opacity:1},800)
+$('#consulting #textWrap a').delay(500).animate({opacity:1},800)
+
+$('#consulting #textWrap a').hover(function () {
+  $('#consulting #textWrap a > div').stop().animate({opacity:1})
+}, function () {
+  $('#consulting #textWrap a > div').stop().animate({opacity:0})
+})
+
+// section2
+$('#cescoInfo article a').hover(function () {
+  $('#cescoInfo article a > div').stop().animate({opacity:1})
+}, function () {
+  $('#cescoInfo article a > div').stop().animate({opacity:0})
+})
+// section3
+$('#serviceWrap > a').hover(function () {
+  $('#serviceWrap > a > div').stop().animate({opacity:1})
+}, function () {
+  $('#serviceWrap > a > div').stop().animate({opacity:0})
+})
+//
+$("#sliderList").prepend($("#sliderList li:last"))
+let slider = function() {
+  $("#sliderList li").not($("#sliderList li:eq(1)")).css({ opacity: 0.5 })
+  $('.next').on('click', function() {
+  $("#sliderList:not(:animated)").animate({ marginLeft: `-=700` }, 600, function() {
+    $(this).append($(">li:first", this)).css({ marginLeft: -700 },600)
+  })
+  $("#sliderList li:eq(1):not(:animated)").animate({ width: 500, height: 300, marginTop: 100, opacity: 0.5},600)
+  $("#sliderList li:eq(1) div:not(:animated)").animate({ width: 500, height: 250, borderRadius: 15},600)
+  $("#sliderList li:eq(1) a:not(:animated)").animate({ fontSize:15},600)
+  $("#sliderList li:eq(2):not(:animated)").animate({ width: 1000, height: 600, marginTop: 0, opacity: 1 },600)
+  $("#sliderList li:eq(2) div:not(:animated)").animate({ width: 1000, height: 500, borderRadius: 30},600)
+  $("#sliderList li:eq(2) a:not(:animated)").animate({ fontSize:30},600)
+  })
+  $(".prev").on('click', function() {
+  $("#sliderList:not(:animated)").prepend($("#sliderList li:last")).css({ marginLeft: -1400 })
+  $("#sliderList li:eq(2) a").css({fontSize:30},600)
+  $("#sliderList li:eq(2)").css({ width: 1000, height: 600, marginTop: 0, opacity: 1 })
+  $("#sliderList li:eq(2) div").css({ width: 1000, height: 500, borderRadius: 30})
+
+
+  $("#sliderList li:eq(1):not(:animated)").animate({ width: 1000, height: 600, marginTop: 0, opacity: 1 },600)
+  $("#sliderList li:eq(1) div:not(:animated)").animate({ width: 1000, height: 500, borderRadius: 30},600)
+  $("#sliderList li:eq(1) a:not(:animated)").animate({fontSize:30},600)
+  $("#sliderList li:eq(2):not(:animated)").animate({ width: 500, height: 300, marginTop: 100, opacity: 0.5 },600)
+  $("#sliderList li:eq(2) div:not(:animated)").animate({ width: 500, height: 250, borderRadius: 15},600)
+  $('#sliderLIst li:eq(2) a:not(:animated)').animate({fontSize:15})
+  $("#sliderList:not(:animated)").animate({ marginLeft: `+=700`},600)
+  })
+}
+let mobileSlider = function () {
+}
+let sliderAction = function () {
+  if ( $(window).innerWidth() < 1000 ) {
+    $('.next').off();
+    $('.prev').off();
+    mobileSlider();
+  }
+  else {
+    slider();
+  }
+  }
+  sliderAction();
+  $(window).on('resize', function () {
+    sliderAction();
+  })
+// section4
+
+let member = function() {
+$('#membersWrap > div').on('mouseenter', function () {
+  $(this).stop().animate({marginTop:-20},0.01);
+  $('h3', this).stop().animate({paddingTop: 350},400)
+  $('div', this).stop().animate({opacity:1})
+})
+$('#membersWrap > div').on('mouseleave', function () {
+  $(this).stop().animate({marginTop:0,},10)
+  $('h3', this).stop().animate({paddingTop: 500},600)
+  $('div', this).stop().animate({opacity:0});
+})
+}
+let memberAction = function() {
+if ( $(window).innerWidth() < 1000 ) {
+  $("#membersWrap > div").off();
+  $("#membersWrap > div").css({ marginTop: 0 })
+  $('#membersWrap > div h3').css({paddingTop: 350})
+  $('#membersWrap > div div').css({opacity:1});
+}
+else {
+  member();
+  $("#membersWrap > div").css({ marginTop: -20 })
+  $('#membersWrap > div h3').css({paddingTop: 500})
+  $('#membersWrap > div div').css({opacity:0});
+}
+}
+memberAction();
+$(window).on('resize', function() {
+memberAction();
+})
+// section5
+// footer
---
+++ js/sub.js
@@ -0,0 +1,28 @@
+$('#clientSystem ul li').hover(function () {
+    $(this).css({borderColor: '#0074b7'});
+    $(this).stop().animate({paddingTop:10});
+    $('>p', this).css({color:'#0074b7'});
+    $('>p:last', this).stop().animate({opacity:1});
+}, function () {
+    $(this).css({borderColor: '#dedede'});
+    $(this).stop().animate({paddingTop:30});
+    $('>p', this).css({color:'#000'});
+    $('>p:last', this).stop().animate({opacity:0});
+})
+//
+$('#solStep ul li').on('click', function () {
+  $('#solStep ul li').removeClass('on')
+  $(this).addClass('on')
+})
+$('#solStep ul li:eq(0)').on('click', function () {
+  $('#stepWrap ul').css({marginLeft:0})
+})
+$('#solStep ul li:eq(1)').on('click', function () {
+  $('#stepWrap ul').css({marginLeft:-540})
+})
+$('#solStep ul li:eq(2)').on('click', function () {
+  $('#stepWrap ul').css({marginLeft:-1080})
+})
+$('#solStep ul li:eq(3)').on('click', function () {
+  $('#stepWrap ul').css({marginLeft:-1620})
+})
